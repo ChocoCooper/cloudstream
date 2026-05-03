7b099642a9f17b7c199f47d86580dfb1a44c324b
Harish K <kpharish2001@gmail.com>
2026-05-03 14:51:03 +0530

    Update build.gradle.kts

--- YoTurkish/build.gradle.kts
+++ YoTurkish/build.gradle.kts
@@ -1,4 +1,16 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 version = 3
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
 
 cloudstream {
     authors     = listOf("ChocoCooper")
