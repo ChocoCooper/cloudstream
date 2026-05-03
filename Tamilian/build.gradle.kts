bf7e3af37f9072ca67b351e5e1b386835b873975
Harish K <kpharish2001@gmail.com>
2026-05-03 12:48:43 +0530

    Create build.gradle.kts

---
+++ Tamilian/build.gradle.kts
@@ -0,0 +1,28 @@
+// use an integer for version numbers
+version = 2
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    language = "ta"
+    // All of these properties are optional, you can safely remove them
+    description = "Movies (Tamil)"
+    authors = listOf("NivinCNC", "ChocoCooper")
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movies"
+    )
+
+    iconUrl = "https://tamilian.io/favicon.ico"
+}
