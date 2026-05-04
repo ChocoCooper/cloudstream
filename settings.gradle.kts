32e400bfc7cba45e50ff0fb7c802a7ff6a824f27
Harish K <kpharish2001@gmail.com>
2026-05-04 17:35:13 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,19 @@
-// https://developer.android.com/build#settings-file
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
+rootProject.name = "CloudstreamPlugins"
+
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+
+val disabled = listOf("__Temel", "__PlayerTest", "ExampleProvider")
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-        maven("https://jitpack.io")
-    }
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-rootProject.name = "CloudStream"
-include(":Tamilian", ":YoTurkish")
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
