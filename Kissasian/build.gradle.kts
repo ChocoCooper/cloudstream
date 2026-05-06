8a479767fff97bc57d046c1ac5d51f71586aadc2
Harish K <kpharish2001@gmail.com>
2026-05-06 17:23:18 +0530

    Create build.gradle.kts

---
+++ Kissasian/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("ChocoCooper")
+    language    = "en"
+    description = "KissAsian - Stream & Download Asian Dramas and Movies in HD"
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movies, TvSeries, AsianDrama") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    iconUrl = "https://kissasian.cam/favicon.ico"
+}
